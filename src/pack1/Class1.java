0d81d12bde7d457a617d13b5feb46ae1840d54b4
HP <HP@LAPTOP-C0D3J0H0>
2023-03-08 15:49:05 +0530

    push Message

--- src/pack1/Class1.java
+++ src/pack1/Class1.java
@@ -1,5 +1,10 @@
 package pack1;
 
-public class Class1 {
-
+public class Class1 
+{
+public static void main(String[] args) 
+{
+	System.out.println("Hello");
+	System.out.println("hello-Harish");
+}
 }
